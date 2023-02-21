d5043605a2e9bc79756b032e91f1878091adab68
abiudbenitez <abiudmisael10@hotmail.com>
2023-02-21 00:21:27 -0600

    solo se cambio de ubicacion

--- UsoDeBotones.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class UsoDeBotones {
-    public static void main(String[] args) {
-        String[] options={"Terminal", "1", "2", "3" , "4", "5"};
-        int v = JOptionPane.showOptionDialog(null, "Ingresa un numero entero del 1 al 5", "Numeros", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-        JOptionPane.showMessageDialog(null, "Boton seleccionado: " + v);
-    }
-}
