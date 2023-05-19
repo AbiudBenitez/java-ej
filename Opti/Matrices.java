08797210b8991087c311c7ac7c027ff6b8efc9d7
abiudbenitez <abiudmisael10@hotmail.com>
2023-05-18 19:07:25 -0600

    Se intento hacer el codigo

--- Opti/Matrices.java
+++ Opti/Matrices.java
@@ -5,33 +5,35 @@ public class Matrices {
     Scanner sc = new Scanner(System.in);
     int[][] matriz;
     int[] res;
-    int numero, i, j, temp = 0;
+    int[] pos;
+    int numero, i, j, temp, con;
+    temp = con = 0;
     System.out.println("Ingrese el numero de nodos: ");
     numero = sc.nextInt();
     matriz = new int[numero][numero];
     res = new int[numero];
+    pos = new int[numero];
     for (i = 0; i < numero; i++) {
       for (j = 0; j < numero; j++) {
         System.out.println("Ingrese el valor del nodo " + (char) ('A' + i) + " con el nodo " + (char) ('A' + j));
         matriz[i][j] = sc.nextInt();
       }
     }
-    while (temp < numero) {
-      for (i = 0; i < numero; i++) {
-        for (j = 0; j < numero; j++) {
-          for (int k = 0; k < numero; k++) {
-            if (i == res[k]) {
-              i++;
-            } else if (j == res[k]) {
-              j++;
-            }
-          }
-          //Aqui empieza el codigo de comparacion 
-          if (temp > matriz[i][j] && temp == 0) {
-            temp = matriz[i][j];
-          }
+    while(temp<numero) {
+
+      for(i = 0; i < numero; i++) {
+        if(res[temp] > matriz[con][i] && matriz[con][i] > 0 || res[temp] == 0) {
+          res[temp] = matriz[con][i];
+          con = i;
         }
-      }
       temp++;
     }
-}}
+
+
+
+    for (j = 0; j < numero; j++) {
+      System.out.println(res[j]);
+    }
+  }
+  }
+}
