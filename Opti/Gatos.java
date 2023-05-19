3d1cd2ddaa29ad6edb5322c6a05b78574697a2ad
abiudbenitez <abiudmisael10@hotmail.com>
2023-05-18 19:06:48 -0600

    Se agregaron mas datos

--- Opti/Gatos.java
+++ Opti/Gatos.java
@@ -4,21 +4,29 @@ public class Gatos {
   public static void main(String[] args) {
     int SMP[];
     float SRD[];
-    int CDC[];
+    int CDC[], Mode[];
+    float X[], Y[];
     boolean SPC[];
-    int cats;
+    int cats, i;
 
     
     Scanner sc = new Scanner(System.in); 
     System.out.print("Ingresa la cantidad de gatos que desee: ");
     cats = Integer.parseInt(sc.nextLine());
 
+    //Declaracion de las variables 
+
     SMP = new int[cats];
     SRD = new float[cats];
     CDC = new int[cats];
     SPC = new boolean[cats];
-    
-    for(int i = 0; i < cats; i++) {
+    X = new float[cats];
+    Y = new float[cats];
+    Mode = new int[cats];
+
+    //Lectura de los datos iniciales
+
+    for(i = 0; i < cats; i++) {
       System.out.print("Defina la catidad de memoria del gato " + (i + 1) + ": ");
       SMP[i] = Integer.parseInt(sc.nextLine());
       System.out.println("Declare la relacion mutativa para las dimensiones seleccionadas del gato " + (i+1) + " :");
@@ -27,7 +35,29 @@ public class Gatos {
       CDC[i] = Integer.parseInt(sc.nextLine());
       System.out.println("Declare si la posicion del gato " + (i+1) + " es candidato a pasar (true o false): ");
       SPC[i] = Boolean.parseBoolean(sc.nextLine());
-      System.out.println(SPC[i]);
+      System.out.print("Declare la posicion del gato " + (i+1) + " en X: ");
+      X[i] = Float.parseFloat(sc.nextLine());
+      System.out.print("Declare la posicion del gato " + (i+1) + " en Y: ");
+      Y[i] = Float.parseFloat(sc.nextLine());
+      System.out.print("Declare el modo del gato " + (i+1) + " (<1=SM : >1=TM): ");
+      Mode[i] = Integer.parseInt(sc.nextLine());
     }
+
+    //Declaracion de la ecuacion que representa al problema
+    
+
+    //Aqui se inicia el proceso de optimizacion con los gatos en SM
+    for(i = 0; i < cats; i++) {
+      if(Mode[i] > 1) {
+
+
+      }else {
+
+      }
+    }
+  }
+
+  public float Ecuacion (float X,float Y) {
+    
   }
 }
